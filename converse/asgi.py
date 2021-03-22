4b186f350ac1c0b70f611a6a8e5000d1d3f1bc46
28-08-2000 <dharamsinghmeena2000@gmail.com>
2021-03-22 19:43:07 +0530

    working friend system + quickchat + room

--- converse/asgi.py
+++ converse/asgi.py
@@ -16,15 +16,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "converse.settings")
 
 # asgi application for routing of web sockets
 application = ProtocolTypeRouter({
-	# to handle traditional http request
-  	"http": get_asgi_application(),
+  # to handle traditional http request
+    "http": get_asgi_application(),
 
-  	# web socket chat handler
-  	"websocket": AuthMiddlewareStack(
+    # web socket chat handler
+    "websocket": AuthMiddlewareStack(
         URLRouter([
-        		url(r'ws/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
-        		url(r'ws/quickchat/(?P<room_name>\w+)/$', CC.as_asgi()),
-        	]
+            url(r'ws/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+            url(r'ws/quickchat/(?P<room_name>\w+)/(?P<user_handle>\w+)/$', CC.as_asgi()),
+          ]
 
             # room.routing.websocket_urlpatterns,
             # does not with different routing
