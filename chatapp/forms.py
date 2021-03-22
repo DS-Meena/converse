601ef3c84d214c5c4fcaf88271621431fca5d0ac
28-08-2000 <dharamsinghmeena2000@gmail.com>
2021-03-22 15:57:16 +0530

    reverting

--- chatapp/forms.py
+++
@@ -1,7 +0,0 @@
-# creating a form for user handle and user room
-from django import forms
-
-class quickForm(forms.Form):
-	user_handle = forms.CharField(label='User handle', max_length=100)
-	
-	room = forms.CharField(label='Room name', max_length=100)
